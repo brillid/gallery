21236749d0e563bea6091f16dd44a43eafdcc7f1
g0spon <bricha.prude@gmail.com>
2021-11-29 23:32:33 +0100

    testing query method

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -14,7 +14,12 @@ class Database
 
     public function open_db_connection()
     {
-        $this->$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+        if (mysqli_connect_errno())
+        {
+            die("Database connection failed" . mysqli_error());
+        }
     }
 
     public function query($sql)
